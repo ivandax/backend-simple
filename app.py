36c80525ab289fddcf55fcd011bdc93566690acc
ivan.dario <ivan.dario@nexiona.com>
2022-06-04 11:29:46 +0200

    Update migration for project and add post project endpoint

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, jsonify
+from flask import Flask, render_template, request, Response, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 # Add migration config
 migrate = Migrate(app, db)
 
-from database.models import Organization, Profile
+from database.models import Organization, Profile, Project
 from auth.auth import AuthError, requires_auth
 
 
@@ -36,6 +36,29 @@ def test():
             "action": "test"
         }
     )
+
+@app.route("/projects", methods=["POST"])
+@requires_auth("post:projects")
+def add_project(jwt):
+    body = request.get_json()
+    if body:
+        name = body.get("name", None)
+        status = body.get("status", None)
+        created_by = body.get("created_by", None)
+        if name != None and status != None and created_by != None:
+            try:
+                project = Project(name=name, status=status, created_by=created_by)
+                project.insert()
+                return jsonify({
+                    "success": True,
+                })
+            except:
+                print(sys.exc_info())
+                abort(400)
+        else:
+            abort(422)
+    else:
+        abort(422)
 
 #----------------------------------------------------------------------------#
 # Launch.
