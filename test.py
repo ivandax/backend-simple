7a07a0cb7b1e2a3bdbdbdf3576cf684dea1bc41b
ivan.dario <ivan.dario@nexiona.com>
2022-06-18 13:10:46 +0200

    Extend testing file

--- test.py
+++ test.py
@@ -20,6 +20,8 @@ class TriviaTestCase(unittest.TestCase):
 
         self.projectPayload =  {"name": "TestProject6699", "status": "active"}
         self.taskPayload =  {"title": "TestTask199", "description": "My description1199", "project_id": 1}
+        self.projectPatchPayload =  {"status": "inactive"}
+        self.taskPatchPayload =  {"description": "somedesc345"}
         self.validToken = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilc3QkVMUUZIVHE1ODdseS1LemxIXyJ9.eyJpc3MiOiJodHRwczovL2Rldi1pZXRvdmgzZi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjJhZGEyMDdmMzNmNmQ5NzQ2MDZmMjkyIiwiYXVkIjoic2ltcGxldGFzayIsImlhdCI6MTY1NTU0NjQ4NSwiZXhwIjoxNjU1NTUzNjg1LCJhenAiOiIwZk1FSkl0RDZRSFhVblcxeU93eVpYUjcyRXdPM0ZUaSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnRhc2tzIiwiZ2V0OnByb2plY3RzIiwiZ2V0OnRhc2tzIiwicG9zdDpwcm9qZWN0cyIsInBvc3Q6dGFza3MiLCJ1cGRhdGU6cHJvamVjdHMiLCJ1cGRhdGU6dGFza3MiXX0.iWHQOKiB9sB1haqQaKITHqQ8K62bJk0ZlE1VlSzrgvcRtKuw9nC4xxWkyFPmTWpUrhwcXCypJsntmen_R66b0NqHDLXWz_xEb5sOe6vYOBBrfAS7FKcxJB2Py9K_ME0fmgDzWWZ2TgeAfLXAlIG14UUCL0OMndLNZSf3CVzXwGUs4IPyr3zD9bGgvRGM4efUWIXjMG-1lR3UlXeEanKh8DrRTj6tOf1P8E2nVkwnoJLJtFzaGUoSaJQwIlm0pfDkRO8AShUygXsuy-PJ5pGdLFqmVNxQC62rZa-SV3lrF8RwcBMXgjb4OJ5HGkyPX1M9SQd-FfJDqwO3v27sFDmnNA"
     
     def tearDown(self):
@@ -33,6 +35,16 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_post_project_failure(self):
         res = self.client().post("/projects", json=self.projectPayload)
+        data =json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_patch_project_success(self):
+        res = self.client().patch("/projects/1", json=self.projectPatchPayload, headers={"Authorization": self.validToken})
+        data =json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+
+    def test_patch_project_failure(self):
+        res = self.client().patch("/projects/1", json=self.projectPatchPayload)
         data =json.loads(res.data)
         self.assertEqual(res.status_code, 401)
 
@@ -67,6 +79,16 @@ class TriviaTestCase(unittest.TestCase):
         data =json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data["items"])
+
+    def test_patch_task_success(self):
+        res = self.client().patch("/tasks/1", json=self.taskPatchPayload, headers={"Authorization": self.validToken})
+        data =json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+
+    def test_patch_task_failure(self):
+        res = self.client().patch("/tasks/1", json=self.taskPatchPayload)
+        data =json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
